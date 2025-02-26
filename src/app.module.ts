24f914d8863311106b9bb8b8c1bf160750697b91
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-02-26 17:16:21 -0300

    :sparkles: feat: rota login

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserModule } from './api/user/user.module';
 import { BiometriaModule } from './api/biometria/biometria.module';
 import { DocumentModule } from './api/document/document.module';
 import { ClienteModule } from './api/cliente/cliente.module';
+import { LoginModule } from './api/login/login.module';
 
 @Module({
-  imports: [UserModule, BiometriaModule, DocumentModule, ClienteModule],
+  imports: [UserModule, BiometriaModule, DocumentModule, ClienteModule, LoginModule],
   controllers: [],
   providers: [PrismaService],
 })
