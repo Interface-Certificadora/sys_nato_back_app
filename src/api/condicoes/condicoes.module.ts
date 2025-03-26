19dfe5d6a7e41398070fc792b0affc2a8211f30e
Vinicius Victor <116410122+vinevictor@users.noreply.github.com>
2025-03-26 14:44:40 -0300

    update

--- src/api/condicoes/condicoes.module.ts
+++ src/api/condicoes/condicoes.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CondicoesService } from './condicoes.service';
 import { CondicoesController } from './condicoes.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [CondicoesController],
-  providers: [CondicoesService],
+  providers: [CondicoesService, PrismaService],
 })
 export class CondicoesModule {}
